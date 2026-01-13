27372c2e0f5cd3ff574f3bd55e1bfa9753a3543b
Barzeboi <berzenoki@gmail.com>
2026-01-13 09:21:29 -0600

    Spawning nearly there

--- Scripts/UI/hud.gd
+++ Scripts/UI/hud.gd
@@ -8,6 +8,15 @@ var mouse_pos: Vector2
 var placer_texture: CompressedTexture2D = preload("res://Assets/Characters/base_idle_strip9.png")
 var lumberjack: PackedScene = preload("res://Scenes/Characters/lumberjack.tscn")
 @onready var wait_spot: Marker2D = $"../wait_spot"
+
+enum Selected_Purchaseable {
+	LUMBERJACK,
+	PLANTER,
+	COURIER,
+	LUMBERMILL
+}
+
+var currently_seletected: Selected_Purchaseable = Selected_Purchaseable.LUMBERJACK
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -18,6 +27,11 @@ func _process(delta: float) -> void:
 	mouse_pos = get_viewport().get_mouse_position()
 	if is_instance_valid(placer):
 		placer.position = mouse_pos
+		
+	if Input.is_action_just_pressed("Interact"):
+		match currently_seletected:
+			Selected_Purchaseable.LUMBERJACK:
+				_spawn(lumberjack)
 	
 func _physics_process(delta: float) -> void:
 	pass
@@ -31,7 +45,6 @@ func _on_lumberjack_button_pressed() -> void:
 	if is_instance_valid(placer):
 		_delete_placement_visualizer()
 	_create_placement_visualizer(placer_texture)
-	_spawn(lumberjack)
 	
 
 func _create_placement_visualizer(visual: CompressedTexture2D):
@@ -47,6 +60,8 @@ func _delete_placement_visualizer():
 		placer.free()
 		
 func _spawn(instance:PackedScene):
-	var spawn = instance.instantiate()
-	spawn.global_position = wait_spot.global_position
-	owner.add_child(spawn)
+	if is_instance_valid(instance):
+		await get_tree().process_frame
+		var spawn = instance.instantiate()
+		spawn.global_position = mouse_pos
+		owner.add_child(spawn)
